8f8c144099fce1e44bfeb3f28248a2a9e3e730c7
Pyvle <geenby@gmail.com>
2023-09-18 18:52:04 +0300

    abc

---
+++ Прог на языке Джава/TestBall.java
@@ -0,0 +1,34 @@
+public class TestBall {
+    public static void main(String[] args) {
+        Ball ball1 = new Ball(5.0, "Red");
+        Ball ball2 = new Ball(3.0);
+        Ball ball3 = new Ball("Blue");
+        Ball ball4 = new Ball();
+
+        System.out.println("Ball 1:");
+        System.out.println(ball1);
+        System.out.println();
+
+        System.out.println("Ball 2:");
+        System.out.println(ball2);
+        System.out.println();
+
+        System.out.println("Ball 3:");
+        System.out.println(ball3);
+        System.out.println();
+
+        System.out.println("Ball 4:");
+        System.out.println(ball4);
+        System.out.println();
+
+        // Test getter and setter methods
+        System.out.println("Changing properties of Ball 1:");
+        ball1.setRadius(6.0);
+        ball1.setColor("Green");
+
+        System.out.println("Updated Ball 1:");
+        System.out.println("Radius: " + ball1.getRadius());
+        System.out.println("Color: " + ball1.getColor());
+        System.out.println("Volume: " + ball1.volume());
+    }
+}
