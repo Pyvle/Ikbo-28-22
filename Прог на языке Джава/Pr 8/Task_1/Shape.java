e9e39f2a3e8ba7a93d9ce34fb856fd08040e54fd
Pyvle <geenby@gmail.com>
2023-10-23 23:11:09 +0300

    ft

---
+++ Прог на языке Джава/Pr 8/Task_1/Shape.java
@@ -0,0 +1,11 @@
+package Task_1;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public abstract class Shape{
+    Point p;
+    public Shape(double x, double y) {
+        p = new Point(x,y);
+    }
+}
